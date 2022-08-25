55e58ed88028e02fc4ee3c137b5ef22bd195f697
IvinoDev <kayantao@protonmail.com>
2022-08-25 09:15:38 +0000

    Création du projet

---
+++ src/main/java/ml/odk/errornotesapi/ErrorNotesApiApplication.java
@@ -0,0 +1,13 @@
+package ml.odk.errornotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorNotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorNotesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ml/odk/errornotesapi/ErrorNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.odk.errornotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorNotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
